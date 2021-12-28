ba15818d80352ec05f00f643ef21bab2d2dca1e4
徐倩岚 <xuqianlan@xuqianlandeMBP.lan>
2021-12-28 13:54:39 +0800

    init

---
+++ status/OneViewController.swift
@@ -0,0 +1,31 @@
+//
+//  OneViewController.swift
+//  status
+//
+//  Created by 徐倩岚 on 2021/12/28.
+//
+
+import UIKit
+
+class OneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+    
+//    override var prefersStatusBarHidden: Bool {
+//        return true
+//    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
